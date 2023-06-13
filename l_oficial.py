aad023ab9999cacb3b198efa4a9334a686515d75
Tech1j <135765952+Tech1j@users.noreply.github.com>
2023-06-13 13:06:06 -0300

    Add files via upload

---
+++ l_oficial.py
@@ -0,0 +1,44 @@
+from time import sleep
+
+sleep(1)
+print('\nOi💌')
+
+sleep(1)
+print('''\nEu to querendo te falar isso há um tempo. 
+E essa foi a melhor forma que eu achei de te contar isso. 
+Então, ta ai😊🍭''')
+sleep(4)
+
+print('''\n\033[31mEu gosto de você.\033[m
+Essa foi a melhor forma de começar a escrever que eu encontrei. 
+Eu sei que esse sentimento não é recíproco, porém eu precisava te falar isso.''')
+sleep(8)
+
+print('''\nEssa mensagem, provavelmente, vai ser bem estranha pra você, 
+já que "somos amigos", mas depois que você, supostamente, descobriu o que eu sinto por você, eu me afastei.''')
+sleep(5)
+
+print('''\nNão consigo olhar pra você e não pensar no quão \033[34mincrível você é\033[m. O pouco que conheço de você me encantou, me fez gostar de você desde a primeira vez que te vi. 
+Seu jeito fofo e atencioso, seu sorriso, sua compreensão com as coisas, sua beleza, cada detalhe seu, 
+e muito mais, que eu não sei expressar em palavras, mas que, de verdade, eu sinto quando te vejo 😊''')
+
+sleep(10)
+print('''\nMe afastei por quê não tive coragem de olhar pra você e perguntar: 
+Você acha que se a gente conversar mais e se conhecer melhor, um dia podemos ser mais que amigos? 
+Ou, simplesmente, dizer que gosto de você na sua frente, 
+já que eu nem sei qual foi sua real reação ao descobrir isso, e, novamente, 
+eu perdi a coragem de expressar e demonstrar qualquer coisa que poderia me dar uma chance de, no mínimo, ser sua amiga.''')
+sleep(10)
+
+print('''\nE agora estou eu aqui, por meio de um código, tomando meia coragem para falar o que eu sinto, já que nem coragem de dizer quem eu sou, estou tendo. 
+Basicamente, estou contanto com a sua inteligência e raciocínio para descobrir quem está codando isso.''')
+sleep(10)
+
+print('''\nE novamente a falta de coragem vem, com o pedido de que ninguém saiba desse código, além de você.''')
+sleep(10)
+
+print('''\nPor fim, gostaria de pedir para você fingir que não leu isso, se a sua resposta a seguinte questão for negativa, se ela for um talvez ou for positiva, entre em contato comigo, pois acho que após essa mensagem, pelo menos uma vez, eu preciso ter coragem de falar sobre isso olhando para você. 
+Agora, você acha que se a gente conversar mais e se conhecer melhor, um dia podemos ser mais que amigos?''')
+
+sleep(10)
+print('\nFoi isso 🚗!!')
